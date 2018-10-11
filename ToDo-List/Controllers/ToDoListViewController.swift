9848c044718c2ef4280716ff34a8aed7bcdc362f
Eric Sans Alvarez <cocoataster@gmail.com>
2018-10-11 17:40:53 +0200

    Add Item model and refactor. Still using UserDefaults, not persistent

--- ToDo-List/Controllers/ToDoListViewController.swift
+++ ToDo-List/Controllers/ToDoListViewController.swift
@@ -10,13 +10,22 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-	var itemArray = [String]()
+	var itemArray = [Item]()
 	let defaults = UserDefaults.standard
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-		guard let items = defaults.array(forKey: "ToDoListArray") as? [String] else { return }
+		
+		let newItem = Item(title: "Find Mike!")
+		itemArray.append(newItem)
+		
+		let newItem2 = Item(title: "Find Mike!")
+		itemArray.append(newItem2)
+		
+		let newItem3 = Item(title: "Find Mike!")
+		itemArray.append(newItem3)
+		
+		guard let items = defaults.array(forKey: "ToDoListArray") as? [Item] else { return }
 		itemArray = items
 	}
 	
@@ -28,7 +37,12 @@ class ToDoListViewController: UITableViewController {
 	
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath)
-		cell.textLabel?.text = itemArray[indexPath.row]
+		
+		let item = itemArray[indexPath.row]
+		
+		cell.textLabel?.text = item.title
+		
+		cell.accessoryType = item.done ? .checkmark : .none
 		
 		return cell
 	}
@@ -36,14 +50,13 @@ class ToDoListViewController: UITableViewController {
 	//MARK: - TableView Delegate Methods
 	
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		
+		itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+		
+		tableView.reloadData()
+		
 		//Avoid gray area selected
 		tableView.deselectRow(at: indexPath, animated: false)
-		
-		if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark) {
-			tableView.cellForRow(at: indexPath)?.accessoryType = .none
-		} else {
-			tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-		}
 	}
 	
 	//MARK: - Add New Items
@@ -57,7 +70,7 @@ class ToDoListViewController: UITableViewController {
 		let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
 			//Save Text
 			guard (textField.text != "") else { return }
-			self.itemArray.append(textField.text!)
+			self.itemArray.append(Item(title: textField.text!))
 			
 			self.defaults.set(self.itemArray, forKey: "ToDoListArray")
 			
---
+++ ToDo-List/Data Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ToDo-List
+//
+//  Created by Eric Sans Alvarez on 11/10/2018.
+//  Copyright © 2018 Eric Sans Alvarez. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+	let title : String
+	var done = false
+	
+	init(title: String) {
+		self.title = title
+	}
+}
