3b763d3ca01cb2d11643a19476c800cf513e3f21
Eric Sans Alvarez <cocoataster@gmail.com>
2018-10-13 11:54:28 +0200

    category view controller set-up

---
+++ ToDo-List/Controllers/CategoryViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CategoryTableViewController.swift
+//  ToDo-List
+//
+//  Created by Eric Sans Alvarez on 13/10/2018.
+//  Copyright © 2018 Eric Sans Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+	@IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+	}
+	
+
+}
